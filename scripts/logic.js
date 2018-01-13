5deef2533ded334cda0b719f68f503a6f3baf091
Owen Black <owenblack@Owens-MacBook-Air.local>
2018-01-13 13:00:47 -0700

    Edit

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,14 +1,52 @@
 function fireMissle(displayBoard, board, coordinates) {
-  return displayBoard
+
+
+var yarray= board[coordinates.y]
+console.log(board)
+var ThingInBoardAtPos= yarray[coordinates.x]
+
+var HitOrMiss= 0
+if (ThingInBoardAtPos>0){
+  HitOrMiss= -1
+} else{  
+  
+  HitOrMiss= 1
+}
+
+displayBoard[coordinates.y][coordinates.x]=HitOrMiss
+var UpdateDB= displayBoard[coordinates.y][coordinates.x]
+
+
+return displayBoard
 }
 
 function generateComputerMove(board) {
+  var x= Math.floor(Math.random() * 10)
+  var y= Math.floor(Math.random() * 10)
   return {
-    x: 0,
-    y: 0
+    x ,
+    y
   }
 }
 
 function isWon(board) {
+
+  var totalhits= 0
+for(let i =0;i < board.length;i++){
+  for(let j =0;j <board[i].length;j++){
+    if(board[i][j]=== -1)
+   totalhits+=board[i][j]
+  }
+}  
+
+console.log(totalhits)
+if (totalhits=== -17){
+  return true;
+}
+
+
+
+
+
   return false
 }
